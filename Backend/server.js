d45fc67edfba666207e10e250226b9109de3a1d0
Rudy <rudymusale@gmail.com>
2026-02-07 10:56:59 +0200

    replace bcrypt with bcryptjs

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { pool } = require('./db');
 const path = require('path');
